5d6ac4842364d1b9bcd82fb41a925971972ceb3b
Ashwani dubey <ashwanidubey@Ashwanis-MacBook-Air.local>
2023-12-12 10:56:52 +0530

    11 dec

--- functions/auth.js
+++ functions/auth.js
@@ -1,9 +1,21 @@
+const {User}=require('../models/User')
+
 const login=(req,res)=>{
-    res.send("hello friends, i am login")
+  const {email,password}=req.body
+  const user= User.find({email,password});
+  if(user.length>=1)
+  {
+     res.send({"status":true,user})
+  }
+  else{
+    res.send({"status":false,"message":"invalid creds"})
+  }
   }
 const signup=(req,res)=>{
-  console.log(req.body)
-    res.send("hello friends, i am signup")
+    const {name,email,password}=req.body
+    const user= User({name,email,password});
+    user.save();
+    res.send("hello friends, i am signup"+user)
   }
 
   module.exports={login,signup}
--- functions/note.js
+++ functions/note.js
@@ -1,8 +1,18 @@
-const createnote=(req,res)=>{
-    res.send("hello friends, i am createnote")
+const {Note}=require('../models/Note');
+
+const createnote=async (req,res)=>{
+    const {title,desc,status}=req.body
+   const note= await Note({title,desc,status});
+   note.save()
+    res.send("hello friends, i am createnote"+note)
+}
+const searchnote=async (req,res)=>{
+    const {title}=req.body
+   const notes= await Note.find({title});
+    res.send("hello friends, i am createnote"+notes)
 }
 const deletenote=(req,res)=>{
     res.send("hello friends, i am delete")
 }
 
-module.exports={createnote,deletenote}
+module.exports={createnote,deletenote,searchnote}
